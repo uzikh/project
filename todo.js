2cf3e3cc3aec2133fb14572eff81776ff483f5c8
uzikh <150172749+uzikh@users.noreply.github.com>
2024-02-02 14:22:58 +0400

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,10 @@
+const form = document.getElementById("form");
+const input =document.getElementById("input");
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    const todo = input.value;
+
+    if(!todo)
+})
